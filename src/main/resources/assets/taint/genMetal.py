5f2e7f812c9a46c31321f12e9ea2db8d9e421550
Jacob Inkrote <jaketheultimate@gmail.com>
2020-04-06 01:03:12 -0400

    resources

--- src/main/resources/assets/taint/genMetal.py
+++ src/main/resources/assets/taint/genMetal.py
@@ -1,5 +1,50 @@
 import sys
 
+metalNames = [
+"accelerase",
+"adamantite",
+"adipatum",
+"aerium",
+"antenium",
+"ardorum",
+"ascalite",
+"atercaeum",
+"betweensteel",
+"black_rosite",
+"caersin",
+"cannium",
+"cibarite",
+"dupondium",
+"escalite",
+"faerite",
+"fierkite",
+"infernium",
+"inurose",
+"littium",
+"lordslime",
+"luginite",
+"lunite",
+"neulite",
+"orichalcum",
+"orinase",
+"oscurum",
+"peractium",
+"piridium",
+"ptemnium",
+"rallium",
+"sestertium",
+"slipsteel",
+"solairite",
+"soldrite",
+"starsteel",
+"superlumium",
+"tempestite",
+"terium",
+"termium",
+"unseelium",
+"urielium"
+]
+
 blockstate = "{{\n  \"variants\": {{\n    \"normal\": [\n      {{ \"model\": \"taint:block_{name}\" }}\n    ]\n  }}\n}}"
 model = "{{\n  \"parent\": \"block/cube_all\",\n  \"textures\": {{\n    \"all\": \"taint:blocks/block_{name}\"\n  }}\n}}"
 item_model = "{{\n  \"parent\": \"taint:block/block_{name}\"\n}}"
@@ -10,7 +55,7 @@ model_f = "./models/block/block_{name}.json"
 item_block_f = "./models/item/block_{name}.json"
 item_ingot_f = "./models/item/ingot_{name}.json"
 item_nugget_f = "./models/item/nugget_{name}.json"
-for string in sys.argv[1:]:
+for string in metalNames:
     f = open(blockstate_f.format(name=string), "w")
     f.write(blockstate.format(name=string))
     f.close()
